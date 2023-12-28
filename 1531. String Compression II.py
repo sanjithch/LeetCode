b6288c54f2de8a497b59d149f1d80e3e6886ee50
sanjithch <sanjith.ch1999@gmail.com>
2023-12-28 12:47:34 -0500

    Update 1531. String Compression II.py
    Reference it again

--- 1531. String Compression II.py
+++ 1531. String Compression II.py
@@ -1,6 +1,7 @@
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
-
+        #using brute force method with cache will solve this
+        # top down Approach
         cache = {}
         def count(i, k, prev, prev_count) -> int:
             if(i, k, prev, prev_count) in cache:
@@ -10,14 +11,14 @@ class Solution:
             if len(s) - i == k:
                 return 0
 
-
+            # if prev equates to the current
             if s[i]==prev:
                 inc = 1 if prev_count in [1, 9, 99] else 0
                 ans = inc + count(i+1, k, prev, prev_count+1)
             else:
                 ans = min(
-                    count(i+1, k-1, prev, prev_count),
-                    1+count(i+1, k, s[i], 1)
+                    count(i+1, k-1, prev, prev_count), # if you delete current
+                    1+count(i+1, k, s[i], 1) # if you do not delete current
                 )
 
             cache[i, k, prev, prev_count] = ans
