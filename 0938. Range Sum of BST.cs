c1410878c0ce4a05cdea6220c0beacd988da5286
sanjithch <sanjith.ch1999@gmail.com>
2024-01-07 21:02:06 -0500

    Create 0938. Range Sum of BST.cs

---
+++ 0938. Range Sum of BST.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    
+    public int RangeSumBST(TreeNode root, int low, int high) {
+        if(root==null) return 0;
+        int sum = 0;
+        if(root.val>=low && root.val<=high){
+            sum += root.val;
+        }
+        sum += RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+        return sum;
+    }
+}
